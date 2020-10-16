0847095f5ddbf1c42316a3f4138eb38851c940bf
leandro <carolino.leandro@gmail.com>
2020-10-16 12:34:39 +0200

    select option added

--- src/app/create-employee.component.ts
+++ src/app/create-employee.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Department } from './models/department.model';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,10 +8,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  departments: Department[] = [
+    {id: 1, name: 'Help Desk'},
+    {id: 2, name: 'HR'},
+    {id: 3, name: 'IT'},
+    {id: 4, name: 'Payroll'}
+  ];
   fullName: string = 'Name';
   email: string = 'Email';
   gender: string = 'female';
   phoneNumber: number;
+  isActive:boolean;
+  department: string = '3';
   constructor() { }
 
   ngOnInit() {
