abad29e10d5c99d1186da4c264e2c7be3cea2ff4
leandro <carolino.leandro@gmail.com>
2020-10-21 09:45:52 +0200

    datepicker config added

--- src/app/create-employee.component.ts
+++ src/app/create-employee.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Department } from './models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
 
 @Component({
   selector: 'app-create-employee',
@@ -8,6 +10,8 @@ import { Department } from './models/department.model';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  datePickersConfig: Partial<BsDatepickerConfig>
+  dateOfBirth: Date = new Date(2020, 0, 30)
   departments: Department[] = [
     {id: 1, name: 'Help Desk'},
     {id: 2, name: 'HR'},
@@ -20,8 +24,16 @@ export class CreateEmployeeComponent implements OnInit {
   phoneNumber: number;
   isActive:boolean;
   department: string = '3';
-  dateOfBirth: string;
-  constructor() { }
+  constructor() {
+    this.datePickersConfig = Object.assign({},
+      {
+        containerClass: 'theme-dark-blue',
+        showWeekNumbers: false,
+        minDate: new Date(2018, 0, 1),
+        maxDate: new Date(2021, 11, 31),
+        dateInputFormat: 'DD/MM/YYYY'
+      });
+  }
 
   ngOnInit() {
   }
