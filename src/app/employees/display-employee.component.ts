548d6e5afa40a41818bb2efee890a7aa96c2dffa
leandro <carolino.leandro@gmail.com>
2020-10-26 17:32:02 +0100

    route params added

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { Employee } from "../models/employee.model";
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: "app-display-employee",
   templateUrl: "./display-employee.component.html",
   styleUrls: ["./display-employee.component.css"],
 })
 export class DisplayEmployeeComponent implements OnInit {
+  private selectedEmployeeId: number;
+
   @Input() employee: Employee;
 
 
-  constructor() {}
+  constructor(private _route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.selectedEmployeeId = +this._route.snapshot.paramMap.get('id');
+  }
 
 }
