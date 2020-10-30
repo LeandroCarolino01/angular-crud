3b7f8d34b294ea3cc75963b72230471710fd3e87
leandro <carolino.leandro@gmail.com>
2020-10-30 16:38:48 +0100

    delete func added

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -74,4 +74,11 @@ export class EmployeeService {
     }
   }
 
+  deleteEmployee(id: number){
+    const i = this.listEmployees.findIndex(e => e.id === id);
+    if (i !== -1){
+      this.listEmployees.splice(i, 1);
+    }
+  }
+
 }
