d7c5b6111dd0bb64496395d2f8bd43578235c730
LeandroCarolino01 <carolino.leandro@gmail.com>
2023-09-01 18:17:39 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { EmpAddEditComponent } from './components/emp-add-edit/emp-add-edit.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-app';
+
+  constructor(private _dialog: MatDialog){}
+
+  openAddEditEmpForm() {
+    this._dialog.open(EmpAddEditComponent);
+  }
 }
