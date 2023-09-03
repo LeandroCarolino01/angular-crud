485cae50d63d608215f347edf3fd18b9518f89cf
LeandroCarolino01 <carolino.leandro@gmail.com>
2023-09-03 22:38:50 +0200

    add db json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -31,7 +35,11 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatRadioModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/emp-add-edit/emp-add-edit.component.ts
+++ src/app/components/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,7 @@
+import { DialogRef } from '@angular/cdk/dialog';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { EmployeeService } from 'src/app/services/employee.service';
 
 @Component({
   selector: 'app-emp-add-edit',
@@ -6,5 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss']
 })
 export class EmpAddEditComponent {
+  empForm: FormGroup;
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate'
+  ]
 
+  constructor(private _fb: FormBuilder, private _empService: EmployeeService, private _dialogRef:DialogRef<EmpAddEditComponent>) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: ''
+    })
+  }
+
+  onFormSubmit(){
+    // if (this.empForm.valid){
+    //   console.log(this.empForm.value)
+    // }
+    // this is a way to test the data coming in
+    if (this.empForm.valid){
+      this._empService.addEmployee(this.empForm.value).subscribe({
+        next: (val: any) => {
+            alert('employee added')
+            this._dialogRef.close();
+        },
+        error:  (err: any) => {
+          console.log(err);
+        }
+      })
+    }
+  }
 }
